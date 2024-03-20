998b13f38ccd508a505ef35ed21b606de7380c7b
zayn-shayq <i200895@nu.edu.pk>
2024-03-20 23:21:42 +0500

    Bug fix

--- flask.py
+++ flask.py
@@ -4,7 +4,6 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load your trained model
 model_path = 'model.pkl'
 with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
@@ -15,15 +14,12 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Ensure the input data is in the correct format
     data = request.get_json(force=True)
-    # Assuming the data is a list of features for prediction
     features = np.array(data['features']).reshape(1, -1)
     
     # Generate predictions
     prediction = model.predict(features)
     
-    # You can add post-processing to the prediction here if needed
     
     return jsonify({'prediction': prediction.tolist()})
 
